c3b5342239de1a41884e072026ce7661a44bf42a
redoy12 <moazzemhosain12@gmail.com>
2020-09-26 22:39:19 +0600

    I have done it

---
+++ round.js
@@ -0,0 +1,34 @@
+// function randomNumber(num) {
+//     const randomNum = Math.random() * num;
+//     const roundNum = Math.round(randomNum);
+//     console.log(roundNum);
+// }
+
+// randomNumber(10000);
+
+
+
+// function roundName(num) {
+//     for (let i = 0; i < 10000; i++) {
+//         const randomNum = Math.random() * num;
+//         const roundNum = Math.round(randomNum);
+//         console.log(roundNum);
+//     }
+// }
+// roundName(10000);
+
+
+
+
+    // const randomNum = Math.random.nextInt(10000);
+    //     const roundNum = Math.round(randomNum);
+    //     console.log(roundNum);
+
+
+    //     const randomNum = Math.round(Math.random()*9000+1000);
+   
+    //  console.log(randomNum);
+
+    const randomNum = Math.random()*9000+1000;
+    const numRound = Math.round(randomNum);
+    console.log(numRound);
